9a81e5d02bae2e60ba04e5b0ef542ae259721859
Patrick Jimison <patrick.jimison@altera.com>
2025-12-10 22:49:23 -0700

    Add unit testing for cache functions:

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -22,12 +22,15 @@ func (c *Cache) Add(key string, val []byte) {
 	createdAt: time.Now(),
 	val: val,
     }
+    //fmt.Printf("cache entry %s created at %v\n", key, newEntry.createdAt)
     c.cache[key] = newEntry
     c.Unlock()
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
+    c.Lock()
     _, exists := c.cache[key]
+    c.Unlock()
     if exists {
 	return c.cache[key].val, true
     }
@@ -36,11 +39,28 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 }
 
 func (c *Cache) reapLoop() {
-    // remove entries older than 'interval'
+    limit := 5 * time.Second
+
+    c.Lock()
+    for k, v := range c.cache { 
+	elapsed := time.Since(v.createdAt)
+	if elapsed > limit {
+	    //fmt.Printf("elapsed time is %v which is greater than limit %v, deleting cache entry %s with creation time %v\n", elapsed, limit, k, v.createdAt)
+	    delete(c.cache, k)
+	}
+    }
+    c.Unlock()
 }
 
 func NewCache(interval time.Duration) *Cache {
     newCache := &Cache{}
     newCache.cache = map[string]cacheEntry{}
+    //newCache.reapLoop()
+    ticker := time.NewTicker(interval)
+    go func() { 
+	for range ticker.C {
+	    newCache.reapLoop()
+	}
+    }()
     return newCache
 }
