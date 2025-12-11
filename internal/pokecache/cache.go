55fc4406999ed341586b41ad56ddf5a4b1ebc128
Patrick Jimison <patrick.jimison@altera.com>
2025-12-11 15:48:05 -0700

    Add cache interval as an argument to NewCache, clean up mutex operations by deferring unlocks, create reap() function which gets called from reapLoop()

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -16,51 +16,47 @@ type Cache struct {
     cache map[string]cacheEntry
 }
 
-func (c *Cache) Add(key string, val []byte) {
+func NewCache(interval time.Duration) *Cache {
+    newCache := &Cache{}
+    newCache.cache = map[string]cacheEntry{}
+
+    go newCache.reapLoop(interval)
+
+    return newCache
+}
+
+func (c *Cache) Add(key string, value []byte) {
     c.Lock()
-    newEntry := cacheEntry{
+    defer c.Unlock()
+    c.cache[key] = cacheEntry{
 	createdAt: time.Now(),
-	val: val,
+	val: value,
     }
-    //fmt.Printf("cache entry %s created at %v\n", key, newEntry.createdAt)
-    c.cache[key] = newEntry
-    c.Unlock()
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
     c.Lock()
-    _, exists := c.cache[key]
-    c.Unlock()
-    if exists {
-	return c.cache[key].val, true
-    }
-
-    return []byte{}, false
+    defer c.Unlock()
+    value, exists := c.cache[key]
+    return value.val, exists
 }
 
-func (c *Cache) reapLoop() {
-    limit := 5 * time.Second
+func (c *Cache) reapLoop(interval time.Duration) {
+    ticker := time.NewTicker(interval)
+    for range ticker.C {
+	c.reap(interval)
+    }
+}
 
+func (c *Cache) reap(interval time.Duration) {
     c.Lock()
+    defer c.Unlock()
     for k, v := range c.cache { 
 	elapsed := time.Since(v.createdAt)
-	if elapsed > limit {
-	    //fmt.Printf("elapsed time is %v which is greater than limit %v, deleting cache entry %s with creation time %v\n", elapsed, limit, k, v.createdAt)
+	if elapsed > interval {
+	    //fmt.Printf("elapsed time is %v which is greater than interval %v, deleting cache entry %s with creation time %v\n", elapsed, interval, k, v.createdAt)
 	    delete(c.cache, k)
 	}
     }
-    c.Unlock()
 }
 
-func NewCache(interval time.Duration) *Cache {
-    newCache := &Cache{}
-    newCache.cache = map[string]cacheEntry{}
-    //newCache.reapLoop()
-    ticker := time.NewTicker(interval)
-    go func() { 
-	for range ticker.C {
-	    newCache.reapLoop()
-	}
-    }()
-    return newCache
-}
