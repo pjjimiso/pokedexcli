5a06a9a19c5c6c34b6d06a942b47203d3ae7c818
Patrick Jimison <patrick.jimison@altera.com>
2025-12-15 21:30:25 -0700

    implement commandExplore function to receive location and call ListPokemon

--- command_explore.go
+++ command_explore.go
@@ -2,13 +2,17 @@ package main
 
 
 import (
-    "fmt" 
-    "errors"
+    "fmt"
 )
 
 
-func commandExplore(cfg *config, location string) error { 
-    pokemon, err := pokeapiClient.ListPokemon(location)
+func commandExplore(cfg *config, args ...string) error { 
+    if len(args) == 0 {
+        return fmt.Errorf("explore requires a location argument")
+    }
+    location := args[0]
+
+    err := cfg.pokeapiClient.ListPokemon(location)
     if err != nil { 
         return err
     }
